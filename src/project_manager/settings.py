7f11d7e3fdea84fde204bd29628cf577c062d026
Thalib Abu Qitaal <thalib74@gmail.com>
2021-08-20 14:04:40 +0700

    create user model

--- src/project_manager/settings.py
+++ src/project_manager/settings.py
@@ -30,6 +30,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'user'
 ]
 
 MIDDLEWARE = [
@@ -116,3 +117,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'user.User'
--- src/user/models.py
+++ src/user/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    USER_ROLE = (
+        ('Admin', 'Admin'),
+        ('HR', 'HR'),
+        ('PM', 'PM'),
+        ('Developer', 'Developer'),
+    )
+    
+    role = models.CharField(choices=USER_ROLE, max_length=50)
