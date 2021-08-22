f88e8c1b49587ea407fe8d588dc166581bb9220e
Thalib Abu Qitaal <thalib74@gmail.com>
2021-08-22 15:10:14 +0700

    create user list feature

--- src/user/urls.py
+++ src/user/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import DashboardView
+from .views import DashboardView, UserListView
 
 app_name = 'user'
 
 urlpatterns = [
     path('dashboard', DashboardView.as_view(), name='user-dashboard'),
+    path('list/', UserListView.as_view(), name='user-list'),
 ]
--- src/user/views.py
+++ src/user/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import User
 
 
 class DashboardView(LoginRequiredMixin, generic.TemplateView):
     template_name = 'user/dashboard.html'
+
+
+class UserListView(LoginRequiredMixin, generic.ListView):
+    template_name = 'user/user_list.html'
+    context_object_name = 'users'
+
+    def get_queryset(self):
+        return User.objects.all()
