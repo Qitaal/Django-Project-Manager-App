062ec2d7712f618bdf44d6b1b36fdd343c5eb0ac
Thalib Abu Qitaal <thalib74@gmail.com>
2021-08-22 13:27:56 +0700

    try use autopep8 formater

--- src/user/views.py
+++ src/user/views.py
@@ -2,5 +2,6 @@ from django.shortcuts import render
 from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
 
+
 class Dashboard(LoginRequiredMixin, generic.TemplateView):
     template_name = 'user/dashboard.html'
