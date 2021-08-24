12a7f310e2510823a8fe38373f54fad0ed1665b5
Thalib Abu Qitaal <thalib74@gmail.com>
2021-08-24 21:14:38 +0700

    create Project and Task model

--- src/project/admin.py
+++ src/project/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from project.models import Project, Task
+
+
+class ProjectTaskInline(admin.TabularInline):
+    model = Task
+    extra = 2
+
+
+class ProjectAdmin(admin.ModelAdmin):
+    inlines = [ProjectTaskInline]
+
+
+admin.site.register(Project, ProjectAdmin)
+admin.site.register(Task)
