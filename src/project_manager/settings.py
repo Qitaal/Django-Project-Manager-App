78ac151d84ded893a77e1cad71d9eb1f623a4ac2
Thalib Abu Qitaal <thalib74@gmail.com>
2021-08-27 15:27:10 +0700

    remove env

--- src/project_manager/settings.py
+++ src/project_manager/settings.py
@@ -2,8 +2,8 @@ import environ
 
 from pathlib import Path
 
-env = environ.Env()
-environ.Env.read_env()
+# env = environ.Env()
+# environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('DJANGO_SECRET_KEY')
+SECRET_KEY = 'django-insecure--7hiod!_*dbx^4ex41(p*(5tex6235lvybdz_4)7gb8xtiko&+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
